bb73d68daca577ba34b8f04830ceb5612d7481ed
foxleren <64990498+foxleren@users.noreply.github.com>
2022-10-31 21:14:37 +0300

    Update handler.go

--- backend/pkg/handler/handler.go
+++ backend/pkg/handler/handler.go
@@ -2,7 +2,6 @@ package handler
 
 import (
 	"github.com/foxleren/GamesOn/backend/pkg/service"
-	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
@@ -20,9 +19,8 @@ func CORS() gin.HandlerFunc {
 		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
 		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
 		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE")
-
 		if c.Request.Method == "OPTIONS" {
-			c.AbortWithStatus(204)
+			c.IndentedJSON(204, "")
 			return
 		}
 
@@ -34,7 +32,7 @@ func (h *Handler) InitRoutes() *gin.Engine {
 	gin.SetMode(gin.ReleaseMode)
 	router := gin.New()
 
-	router.Use(cors.Default())
+	router.Use(CORS())
 
 	auth := router.Group("/auth")
 	{
