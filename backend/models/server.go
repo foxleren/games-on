4724aa94fef24d3fa60ad7afd86c7656e94ec9b1
foxleren <64990498+foxleren@users.noreply.github.com>
2022-11-03 00:24:05 +0300

    Update server.go

--- backend/models/server.go
+++ backend/models/server.go
@@ -2,7 +2,9 @@ package models
 
 import (
 	"context"
+	"crypto/tls"
 	"net/http"
+	"os"
 	"time"
 )
 
@@ -17,9 +19,10 @@ func (s *Server) Run(port string, handler http.Handler) error {
 		MaxHeaderBytes: 1 << 20,
 		ReadTimeout:    10 * time.Second,
 		WriteTimeout:   10 * time.Second,
+		TLSConfig:      &tls.Config{ServerName: os.Getenv("DOMAIN")},
 	}
 
-	return s.httpServer.ListenAndServe()
+	return s.httpServer.ListenAndServeTLS("certificate.crt", "private.key")
 }
 
 func (s *Server) Shutdown(ctx context.Context) error {
