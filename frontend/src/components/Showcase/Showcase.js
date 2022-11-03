5b09d9d5a81ff5ee53f559953cfd6b1c3954b3da
foxleren <64990498+foxleren@users.noreply.github.com>
2022-11-03 04:22:20 +0300

    Update Showcase.js

--- frontend/src/components/Showcase/Showcase.js
+++ frontend/src/components/Showcase/Showcase.js
@@ -16,7 +16,7 @@ const Showcase = observer(() => {
     return (<div className={'showcase-container'}>
         <div className={'showcase-title'}>All games:</div>
         <div className={'showcase-items'}>
-            {game.games == null && game.games.map((item, index) => {
+            {game.games != null && game.games.map((item, index) => {
                 return <ShowcaseCard key={index} id={item.id} name={item.title} description={item.description}
                                      image={getPreview(item.images)}/>
             })}
