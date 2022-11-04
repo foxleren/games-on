8a17a2b0d18fb22b9d5e01282427941ceb9136b5
foxleren <foxleren@gmail.com>
2022-11-04 11:35:04 +0300

    something

--- frontend/src/pages/NotFoundPage/NotFoundPage.jsx
+++ frontend/src/pages/NotFoundPage/NotFoundPage.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
+import './NotFoundPage.scss'
+import {useNavigate} from "react-router-dom";
 
 const NotFoundPage = () => {
+    const navigate = useNavigate()
     return (
-        <>
-            <div>NOT FOUND PAGE</div>
-        </>
+        <div className={'not-found-page'}>
+            <div className={'not-found-page-title'}>
+                NOT FOUND PAGE
+            </div>
+
+            <a onClick={() => navigate(-1)}>Go back</a>
+        </div>
     );
 };
 
